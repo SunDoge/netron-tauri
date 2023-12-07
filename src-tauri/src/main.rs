6a0826dd3836b59e5a9e3f631374b80b6aeb72d5
SunDoge <384813529@qq.com>
2023-12-07 23:56:15 +0800

    save

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,19 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use tauri::{utils::config::AppUrl, WindowUrl};
 
 fn main() {
+    let port = portpicker::pick_unused_port().expect("fail to find unused port");
+
+    let mut context = tauri::generate_context!();
+    let url = format!("http://localhost:{}", port).parse().unwrap();
+    let window_url = WindowUrl::External(url);
+    context.config_mut().build.dist_dir = AppUrl::Url(window_url);
+
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
+        .plugin(tauri_plugin_localhost::Builder::new(port).build())
+        .invoke_handler(tauri::generate_handler![])
+        .run(context)
         .expect("error while running tauri application");
 }
