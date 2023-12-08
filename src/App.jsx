90915013e955c762947d47038f6d2bb4d78af08f
SunDoge <384813529@qq.com>
2023-12-08 08:11:41 +0000

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import "./App.css";
-import "../netron/source/grapher.css";
 
 import Toolbar from "./components/toolbar";
 import Logo from "./components/logo";
@@ -7,33 +6,23 @@ import Graph from "./components/graph";
 import Sidebar from "./components/sidebar";
 import MessageDialog from "./components/message-dialog";
 import Titlebar from "./components/titlebar";
+import { onMount } from "solid-js";
 
 // Preload
 import { View } from "../netron/source/view"
 import { BrowserHost } from "../netron/source/browser"
-import { onMount } from "solid-js";
-
-
-import { resolveResource } from "@tauri-apps/api/path";
-import { readTextFile } from "@tauri-apps/api/fs";
-// import modules from "./modules";
 
 
 function App() {
 
   onMount(async () => {
     const host = new BrowserHost();
-    // host.request = async (file, encoding, base) => {
-    //   console.log(file, encoding, base)
-    //   const resourcePath = await resolveResource('../netron/source/' + file);
-    //   const data = await readTextFile(resourcePath);
-    //   return data;
-    // }
+
     host.require = async (id) => {
       console.log(id);
       return import("." + id + ".js");
     }
-    
+
     window.__view__ = new View(host);
     window.__view__.start();
     console.log('start view')
